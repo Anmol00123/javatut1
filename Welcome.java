e2ec64094a5d2291d35a6cb4873b762988d43b39
Anmol00123 <128777630+Anmol00123@users.noreply.github.com>
2023-05-02 14:25:06 +0530

    Add files via upload

---
+++ Welcome.java
@@ -0,0 +1,117 @@
+
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		
+        //Logical
+        // int a=10 , b=20 , c= 30 ,d= 0 ;
+        // System.out.println("Var1= " + a);
+        // System.out.println("Var2= " + b);
+        // System.out.println("Var3= " + c);
+
+        // if((a>b) || (b==c)){
+        //     d=a+b+c;
+        //     System.out.println("Sum= " + d);
+
+        // }
+        // else{
+        //     System.out.println("Not found");
+        // }
+
+        // 
+
+        // int n= 10;
+        // if(n>0){
+        //     System.out.println("number +ve");
+        // }
+
+        // else{
+        //     System.out.println("number -ve");
+
+        // }
+
+        // int n=5;
+        // for(int i = 1; i<=n ;i++){
+        //     System.out.println("Anmol");
+        // }
+
+
+        // int i=1, n=5;
+        // while(i<=n){
+        //     System.out.println(i);
+        //     i++;
+        // }
+
+        // int n= 44;
+        // String size;
+        // switch(n){
+        //     case 29:
+        //         size="small no";
+        //         break;
+            
+        //     case 42:
+        //         size="medium no";
+        //         break;
+
+        //     case 44:
+        //         size="large no";
+        //         break;
+            
+        //     default:
+        //         size="unknown";
+        //         break;
+        // }
+        // System.out.println("Size" +size);
+
+        // int a=10;
+        // int b= (a>5) ? 20:30
+
+
+        // ****Stringbuffer demo
+
+        // StringBuffer anmol= new StringBuffer("Anmol123");
+
+        // anmol.append("shingari");
+        // System.out.println(anmol);
+        // System.out.println(anmol.capacity());
+        // anmol.insert(0, "john");
+
+        // *****String demo 
+
+        String name= "anmol";
+        System.out.println(name.hashCode());
+        name = name + "shingari";
+        System.out.println(name);
+        String s1 = new String("qwerty");
+        String s2 = new String("qwerty");
+        String s3 = "asdf";
+        String s4 = "asdf";
+        System.out.println(s3 == s4);
+        System.out.println(s3.equals(s4));
+
+
+        System.out.println(s1 == s2);
+        System.out.println(s1.equals(s2));
+
+
+    
+
+        
+
+        
+        
+
+        
+
+
+
+
+        
+
+        
+		
+        
+	}
+
+}
