5a5d6b575109c9d07c951b528a1f97b853772f7d
Anmol00123 <128777630+Anmol00123@users.noreply.github.com>
2023-05-03 16:51:40 +0530

    Add files via upload

---
+++ Exception/ExceptionHandling.java
@@ -0,0 +1,25 @@
+package Exception;
+
+public class ExceptionHandling {
+    public static void main(String[] args) {
+        // try{
+        //     int data=100/0;
+        // }
+        // catch(ArithmeticException e)
+        // {System.out.println(e);}
+
+        // Arrayoutofbound
+        // try{
+        //     int[] number={1,3,5};
+        //     System.out.println(number[10]);
+        // }
+        // catch (Exception e){
+        //     System.out.println("Bad Code");
+
+        // }finally{
+        //     System.out.println("try catch finished");
+        // }
+        
+    }
+    
+}
